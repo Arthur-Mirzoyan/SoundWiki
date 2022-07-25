b0c4bd485033a684a7e94e12b4755cec362d62e9
Emin291 <66358318+Emin291@users.noreply.github.com>
2022-07-25 20:20:01 +0400

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import  BrowsePage  from './Components/BrowsePage/BrowsePage';
 import { SearchPage } from './Components/SearchPage/SearchPage';
+import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,8 +12,8 @@ export default function App() {
     <>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Browse" component={BrowsePage} options={{headerShown:false}} />
-          <Tab.Screen name="Search" component={SearchPage} />
+          <Tab.Screen name="Browse" component={BrowsePage} options={{headerShown:false ,tabBarStyle:{backgroundColor:'#051622'},tabBarLabel:()=>{return null},tabBarIcon: ()=>{return <Image source={{ uri:'https://img.icons8.com/ios-filled/50/1ba098/home.png'}}  style={{width: 25, height: 25}}/>}}} />
+          <Tab.Screen name="Search" component={SearchPage}  options={{tabBarLabel:()=>{return null},tabBarIcon: ()=>{return <Image source={{ uri:'https://img.icons8.com/ios-filled/50/1ba098/search--v1.png' }}  style={{width: 25, height: 25}}/>}}}/>
         </Tab.Navigator>
       </NavigationContainer>
 
