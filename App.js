f83b4f35443c730810d3b2a992bb1e47d4dff85a
dyamo <rikkanavo@gmail.com>
2022-07-28 02:17:09 +0400

    Fixed browse page navigation issues.

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ export default function App() {
     return (
         <NavigationContainer>
             <Tab.Navigator screenOptions={{tabBarHideOnKeyboard: true}}>
-                <Tab.Screen name="Browse" options={{headerShown: false}} component={BrowseStack}/>
-                <Tab.Screen name="Search" options={{headerShown: false}} component={SearchStack}/>
+                <Tab.Screen name="TabBrowse" options={{headerShown: false}} component={BrowseStack}/>
+                <Tab.Screen name="TabSearch" options={{headerShown: false}} component={SearchStack}/>
             </Tab.Navigator>
         </NavigationContainer>
     );
--- Components/Stacks/BrowseStack.jsx
+++ Components/Stacks/BrowseStack.jsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {BrowsePage} from '../BrowsePage/BrowsePage';
 import {GenrePage} from '../GenrePage/GenrePage'
+import {ArtistSinglePage} from "../ArtistSinglePage/ArtistSinglePage";
 
 const Stack = createNativeStackNavigator();
 
-export function SearchStack() {
+export function BrowseStack() {
     return (
         <Stack.Navigator screenOptions={screenOptions}>
             <Stack.Screen options={{title: 'Browse'}} name="Browse" component={BrowsePage}/>
