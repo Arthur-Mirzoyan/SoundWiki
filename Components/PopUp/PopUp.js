8ad57d775ae915850cbe9cf4126b23bab1ef694e
dyamo <rikkanavo@gmail.com>
2022-08-26 23:46:56 +0400

    Updated popup design.

--- Components/PopUp/PopUp.js
+++ Components/PopUp/PopUp.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
-import { Text, View } from "react-native";
-import { Audio } from 'expo-av';
-import { AntDesign } from '@expo/vector-icons';
-import { styles } from './style';
+import React, {useState} from "react";
+import {Image, Text, View} from "react-native";
+import {Audio} from 'expo-av';
+import {AntDesign} from '@expo/vector-icons';
+import {styles} from './style';
 import Modal from "react-native-modal";
 import TextTicker from 'react-native-text-ticker'
 
-export function PopUp({ item, setShowModal, setIsPlaying, showModal, isPlaying }) {
+export function PopUp({item, setShowModal, setIsPlaying, showModal, isPlaying}) {
     const [sound, setSound] = useState();
+    const image = item.album.images[item.album.images.length - 2]
 
     async function playSound(song) {
-        const { sound } = await Audio.Sound.createAsync({ uri: song });
+        const {sound} = await Audio.Sound.createAsync({uri: song}, {shouldPlay: true}, handleStatusChange);
         setSound(sound);
 
         await sound.playAsync();
+    }
+
+    function handleStatusChange(status) {
+        if (status.didJustFinish) {
+            setShowModal(false)
+            setIsPlaying(false)
+        }
     }
 
     return (
@@ -32,39 +40,46 @@ export function PopUp({ item, setShowModal, setIsPlaying, showModal, isPlaying }
                 sound.unloadAsync();
             }}
         >
-            <View style={styles.modalBox} >
+            <View style={styles.modalBox}>
 
-                <TextTicker
-                    style={styles.modalTitle}
-                    duration={5000}
-                    loop
-                    bounce
-                    repeatSpacer={50}
-                    marqueeDelay={1500}
-                >{item.name}</TextTicker>
+                <View>
+                    <Image style={styles.albumImage} source={{uri: image.url}} />
+                </View>
 
-                <AntDesign
-                    name={!isPlaying ? 'play' : 'pausecircle'}
-                    size={40}
-                    color="white"
-                    style={styles.icon}
-                    onPress={() => {
-                        try {
-                            if (isPlaying) {
-                                sound.pauseAsync();
+                <View style={styles.infoBox}>
+                    <TextTicker
+                        style={styles.modalTitle}
+                        duration={5000}
+                        loop
+                        bounce
+                        repeatSpacer={50}
+                        marqueeDelay={1500}
+                    >{item.name}</TextTicker>
+
+                    <Text style={styles.modalText}>
+                        {item.artists.map(artist => artist.name).join(', ')}
+                    </Text>
+                </View>
+
+                <View style={styles.playButtonBox}>
+                    <AntDesign
+                        name={!isPlaying ? 'play' : 'pausecircle'}
+                        size={40}
+                        color="white"
+                        style={styles.playButton}
+                        onPress={() => {
+                            try {
+                                if (isPlaying) {
+                                    sound.pauseAsync();
+                                } else {
+                                    sound.playAsync();
+                                }
+                                setIsPlaying(!isPlaying);
+                            } catch (error) {
                             }
-                            else {
-                                sound.playAsync();
-                            }
-                            setIsPlaying(!isPlaying);
-                        }
-                        catch (error) { }
-                    }}
-                />
-
-                <Text style={styles.modalText}>
-                    {item.artists.map(artist => artist.name).join(', ')}
-                </Text>
+                        }}
+                    />
+                </View>
             </View>
         </Modal>
     )
--- Components/PopUp/style.js
+++ Components/PopUp/style.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-const WIDTH = Dimensions.get('screen').width;
+const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
 
 export const styles = StyleSheet.create({
     modal: {
@@ -9,30 +9,39 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
     },
     modalBox: {
-        width: WIDTH,
+        width: screenWidth,
         height: 'auto',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         backgroundColor: '#98b38d',
         borderTopRightRadius: 50,
         borderTopLeftRadius: 50,
         margin: '-10%',
         padding: 20,
+        paddingHorizontal: 30,
         paddingBottom: 25,
         alignItems: 'center'
     },
+    albumImage: {
+        width: 48,
+        height: 48,
+        borderRadius: 10
+    },
+    infoBox: {
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+        maxWidth: screenWidth / 1.9
+    },
     modalTitle: {
         color: 'white',
-        fontSize: 30,
-        textAlign: 'center',
-        alignSelf: 'center'
+        fontSize: 15
     },
     modalText: {
         color: 'white',
-        fontSize: 24,
-        textAlign: 'center',
-        marginTop: 15
+        fontSize: 12,
     },
-    icon: {
-        alignSelf: 'center',
-        marginTop: 15
+    playButtonBox: {
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
